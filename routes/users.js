ac4453bb3d1c0674e14d58d08668f56a9020a344
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-04 21:53:41 +0500

    API changed

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,27 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const UsersSchema = require('../models/users');
 
-const UsersSchema = require('../models/users')
-
-
-router.get('/all',async (req, res) => {
+// GET all users
+router.get('/all', async (req, res) => {
     try {
-        const users = await UsersSchema.find({})
-        res.send(users)
-    } 
-    catch (error) {
-        res.status(500).json({message: error.message})    
+        const users = await UsersSchema.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
+// POST create a new user
 router.post("/create", async (req, res) => {
-    const user = new UsersSchema(req.body)
+    const user = new UsersSchema(req.body);
+
     try {
-        
-    } 
-    catch (error) {
-            res.status(500).json({message: error.message})    
-    }   
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 
-})
-
-module.exports = router
+module.exports = router;
